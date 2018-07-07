d444287c3b1bde3652c093d2ebe1ef97bc75de5a
DaniDee <DaniDee@Dees-MacBook-Pro.local>
2018-07-07 09:19:21 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://azadmin:asdfgh1@ds129811.mlab.com:29811/azdatabase' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
